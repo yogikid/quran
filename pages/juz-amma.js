756cd5be7024a66dc78254bc832306218c3c2ef3
Rahman <rahwisdilfiqrak@gmail.com>
2024-08-31 01:58:09 +0700

    change current endpoint

--- pages/juz-amma.js
+++ pages/juz-amma.js
@@ -6,60 +6,61 @@ import SearchInput from "@/components/SearchInput";
 import SurahList from "@/components/SurahList";
 
 function JuzAmma({ allSurahList }) {
-  const [searchTerm, setSearchTerm] = useState("");
-  const { colorMode } = useColorMode();
+	const [searchTerm, setSearchTerm] = useState("");
 
-  const surahList = allSurahList.filter((surah) =>
-    surah.name.transliteration.id
-      .toLowerCase()
-      .includes(searchTerm.toLocaleLowerCase())
-  );
+	const { colorMode } = useColorMode();
 
-  return (
-    <>
-      <Head>
-        <title>Al-Quran Juz 30 (Juz Amma)</title>
-        <meta
-          name="description"
-          content="Al-Qur'an 30 Juz beserta Terjemahan Bahasa Indonesia"
-        />
-      </Head>
+	const surahList = allSurahList.filter((surah) =>
+		surah.name.transliteration.id
+			.toLowerCase()
+			.includes(searchTerm.toLocaleLowerCase())
+	);
 
-      <PageHeader title="Juz Amma" goBack />
-      <Box px="15px" pt="20px" as="main">
-        <SearchInput
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Cari surah di Juz Amma"
-        />
+	return (
+		<>
+			<Head>
+				<title>Al-Quran Juz 30 (Juz Amma)</title>
+				<meta
+					name="description"
+					content="Al-Qur'an 30 Juz beserta Terjemahan Bahasa Indonesia"
+				/>
+			</Head>
 
-        {surahList.length === 0 && (
-          <Text
-            fontSize="14px"
-            color={colorMode === "dark" ? "gray.200" : "gray.600"}
-            mt="20px"
-          >
-            Surah yang anda cari tidak ditemukan!
-          </Text>
-        )}
+			<PageHeader title="Juz Amma" goBack />
+			<Box px="15px" pt="20px" as="main">
+				<SearchInput
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+					placeholder="Cari surah di Juz Amma"
+				/>
 
-        <SurahList items={surahList} />
-      </Box>
-    </>
-  );
+				{surahList.length === 0 && (
+					<Text
+						fontSize="14px"
+						color={colorMode === "dark" ? "gray.200" : "gray.600"}
+						mt="20px"
+					>
+						Surah yang anda cari tidak ditemukan!
+					</Text>
+				)}
+
+				<SurahList items={surahList} />
+			</Box>
+		</>
+	);
 }
 
 export default JuzAmma;
 
 export async function getStaticProps() {
-  const res = await fetch("https://api.quran.sutanlab.id/surah/");
-  const resultJson = await res.json();
+	const res = await fetch("https://api.quran.gading.dev/surah/");
+	const resultJson = await res.json();
 
-  const allSurahList = resultJson.data.filter((surah) => surah.number >= 78);
+	const allSurahList = resultJson.data.filter((surah) => surah.number >= 78);
 
-  return {
-    props: {
-      allSurahList,
-    },
-  };
+	return {
+		props: {
+			allSurahList: allSurahList,
+		},
+	};
 }
--- pages/surah/index.js
+++ pages/surah/index.js
@@ -7,60 +7,60 @@ import SearchInput from "@/components/SearchInput";
 import BackToTop from "@/components/BackToTop";
 
 function Surah({ allSurahList }) {
-  const [searchTerm, setSearchTerm] = useState("");
-  const { colorMode } = useColorMode();
+	const [searchTerm, setSearchTerm] = useState("");
+	const { colorMode } = useColorMode();
 
-  const surahList = allSurahList.filter((surah) =>
-    surah.name.transliteration.id
-      .toLowerCase()
-      .includes(searchTerm.toLocaleLowerCase())
-  );
+	const surahList = allSurahList.filter((surah) =>
+		surah.name.transliteration.id
+			.toLowerCase()
+			.includes(searchTerm.toLocaleLowerCase())
+	);
 
-  return (
-    <>
-      <Head>
-        <title>Daftar Surah Al-Quran Beserta Terjemahan | Maos Quran</title>
-        <meta
-          name="description"
-          content="Daftar Surah Al-Quran Beserta Terjemahan dan Tafsir"
-        />
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Daftar Surah Al-Quran Beserta Terjemahan | Maos Quran</title>
+				<meta
+					name="description"
+					content="Daftar Surah Al-Quran Beserta Terjemahan dan Tafsir"
+				/>
+			</Head>
 
-      <PageHeader title="Daftar Surah" goBack />
-      <Box px="15px" pt="20px" as="main">
-        <SearchInput
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Cari surah..."
-        />
+			<PageHeader title="Daftar Surah" goBack />
+			<Box px="15px" pt="20px" as="main">
+				<SearchInput
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+					placeholder="Cari surah..."
+				/>
 
-        {surahList.length === 0 && (
-          <Text
-            fontSize="14px"
-            color={colorMode === "dark" ? "gray.200" : "gray.600"}
-            mt="20px"
-          >
-            Surah yang anda cari tidak ditemukan!
-          </Text>
-        )}
+				{surahList.length === 0 && (
+					<Text
+						fontSize="14px"
+						color={colorMode === "dark" ? "gray.200" : "gray.600"}
+						mt="20px"
+					>
+						Surah yang anda cari tidak ditemukan!
+					</Text>
+				)}
 
-        <SurahList items={surahList} />
-      </Box>
+				<SurahList items={surahList} />
+			</Box>
 
-      <BackToTop />
-    </>
-  );
+			<BackToTop />
+		</>
+	);
 }
 
 export default Surah;
 
 export async function getStaticProps() {
-  const res = await fetch("https://api.quran.sutanlab.id/surah/");
-  const resultJson = await res.json();
+	const res = await fetch("https://api.quran.gading.dev/surah/");
+	const resultJson = await res.json();
 
-  return {
-    props: {
-      allSurahList: resultJson.data,
-    },
-  };
+	return {
+		props: {
+			allSurahList: resultJson.data,
+		},
+	};
 }
